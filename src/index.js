cdd38b6544801a54c4ab2e165d5229cd5bb05790
thiagoribb <thiribeiro142@gmail.com>
2021-12-10 12:04:59 -0300

    Preparando revisão de código

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-
-import "./css/reset.css";
-import "./css/style.css";
-
-const index = document.querySelector(".root");
-ReactDOM.render(<App />, index);
